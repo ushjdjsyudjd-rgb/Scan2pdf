ad21a8ea92b6787d5a54857a21cece6add62f1de
ushjdjsyudjd-rgb <ushjdjsyudjd@gmail.com>
2026-01-03 02:53:51 +0330

    Create ImageAdapter.java

---
+++ app/src/main/java/com/example/scan2pdf/ImageAdapter.java
@@ -0,0 +1,43 @@
+package com.example.scan2pdf;
+
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+    private List<Bitmap> images;
+
+    public ImageAdapter(List<Bitmap> images) {
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_image, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.imageView.setImageBitmap(images.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.itemImageView);
+        }
+    }
+}
