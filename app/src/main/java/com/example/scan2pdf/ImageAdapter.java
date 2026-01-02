56933ba69dfd7b480d1d9e658ffa73be71747ad8
ushjdjsyudjd-rgb <ushjdjsyudjd@gmail.com>
2026-01-03 03:05:17 +0330

    Update ImageAdapter.java

--- app/src/main/java/com/example/scan2pdf/ImageAdapter.java
+++ app/src/main/java/com/example/scan2pdf/ImageAdapter.java
@@ -19,18 +19,21 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // دقت کنید که نام فایل xml درست باشد
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_image, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.imageView.setImageBitmap(images.get(position));
+        if (images.get(position) != null) {
+            holder.imageView.setImageBitmap(images.get(position));
+        }
     }
 
     @Override
     public int getItemCount() {
-        return images.size();
+        return images != null ? images.size() : 0;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
