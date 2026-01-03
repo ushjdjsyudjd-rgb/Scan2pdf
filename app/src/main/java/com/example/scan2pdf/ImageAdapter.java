39681315293e800687b34b3a3afb147e8622c3cf
ushjdjsyudjd-rgb <ushjdjsyudjd@gmail.com>
2026-01-03 03:49:08 +0330

    Update ImageAdapter.java

--- app/src/main/java/com/example/scan2pdf/ImageAdapter.java
+++ app/src/main/java/com/example/scan2pdf/ImageAdapter.java
@@ -11,30 +11,32 @@ import java.util.List;
 
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
     private List<Bitmap> images;
+    private OnItemClickListener listener;
 
-    public ImageAdapter(List<Bitmap> images) {
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    public ImageAdapter(List<Bitmap> images, OnItemClickListener listener) {
         this.images = images;
+        this.listener = listener;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // دقت کنید که نام فایل xml درست باشد
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_image, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        if (images.get(position) != null) {
-            holder.imageView.setImageBitmap(images.get(position));
-        }
+        holder.imageView.setImageBitmap(images.get(position));
+        holder.itemView.setOnClickListener(v -> listener.onItemClick(position));
     }
 
     @Override
-    public int getItemCount() {
-        return images != null ? images.size() : 0;
-    }
+    public int getItemCount() { return images.size(); }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
